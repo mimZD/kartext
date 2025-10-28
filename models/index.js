373d1fc19ea66f8797ed4c3ed6ba46ceb1ad3fbe
mimZD <mim.zahedian@gmail.com>
2025-10-28 19:49:02 +0330

    fix index

--- models/index.js
+++ models/index.js
@@ -1,14 +1,31 @@
+// models/index.js
+const { Sequelize, DataTypes } = require('sequelize');
+const path = require('path');
+
+// تعریف sequelize connection (مطمئن شیم این بخش وجود داره)
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER, 
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    logging: false
+  }
+);
+
+// حالا مدل‌ها رو تعریف کنیم
 const User = require('./User')(sequelize, DataTypes);
 const TimeLog = require('./TimeLog')(sequelize, DataTypes);
-const LeaveRequest = require('./LeaveRequest')(sequelize, DataTypes); // این خط رو اضافه کنید
+const LeaveRequest = require('./LeaveRequest')(sequelize, DataTypes);
 
 const models = {
   User,
   TimeLog,
-  LeaveRequest  // اینم اضافه کنید
+  LeaveRequest
 };
 
-// روابط بین مدل‌ها (اختیاری - اگر نیاز هست)
+// ایجاد روابط اگر نیاز هست
 Object.keys(models).forEach(modelName => {
   if (models[modelName].associate) {
     models[modelName].associate(models);
