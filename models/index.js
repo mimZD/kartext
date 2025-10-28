13d57bebde11cf7bdf2264ae2c6a47ec0ff3a8cf
mimZD <mim.zahedian@gmail.com>
2025-10-28 19:45:43 +0330

    some models realation

--- models/index.js
+++ models/index.js
@@ -1,13 +1,21 @@
-const User = require('./User');
-const Log = require('./Log');
-const sequelize = require('../config/database');
-const LeaveRequest = require('./LeaveRequest')(sequelize, DataTypes);
-// ایجاد ارتباط بدون foreign key constraint برای جلوگیری از خطا
-User.hasMany(Log, { foreignKey: 'userId' });
-Log.belongsTo(User, { foreignKey: 'userId' });
+const User = require('./User')(sequelize, DataTypes);
+const TimeLog = require('./TimeLog')(sequelize, DataTypes);
+const LeaveRequest = require('./LeaveRequest')(sequelize, DataTypes); // این خط رو اضافه کنید
 
-module.exports = {  
+const models = {
   User,
-  Log,
-  sequelize
+  TimeLog,
+  LeaveRequest  // اینم اضافه کنید
 };
+
+// روابط بین مدل‌ها (اختیاری - اگر نیاز هست)
+Object.keys(models).forEach(modelName => {
+  if (models[modelName].associate) {
+    models[modelName].associate(models);
+  }
+});
+
+models.sequelize = sequelize;
+models.Sequelize = Sequelize;
+
+module.exports = models;
